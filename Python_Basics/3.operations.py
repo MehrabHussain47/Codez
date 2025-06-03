5fbaafb761ee0b63b96ff136ed244c0277dde8b3
Asus <mehhussumon47@gmail.com>
2025-06-04 02:26:45 +0600

    Edited code

--- Python_Basics/3.operations.py
+++ Python_Basics/3.operations.py
@@ -1,1 +1,7 @@
 print(3 + 5)
+print(3 - 5)
+print(10 / 5)
+print(3 * 3)    # * => asterisk sign
+print(10 % 3)   # % => modulus sign
+print(2 ** 3)   # ** => double asterisk used for power
+print(10 // 3)  # // => integer division
