0ad99452206d4bec6c387e03480241036b9d3566
Asus <mehhussumon47@gmail.com>
2025-06-05 11:27:31 +0600

    New Folder Added

---
+++ Class_1/03.var_data_type.py
@@ -0,0 +1,15 @@
+# Integer
+age = 24
+print("Age:", age)
+
+# Float
+height = 5.9
+print("Height:", height)
+
+# String
+name = "Alice"
+print("Name:", name)
+
+# Boolean
+is_student = False
+print("Is Student?", is_student)
---
+++ Class_1/04.input_output.py
@@ -0,0 +1,11 @@
+# Taking inputs from user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))    # Converting input to integer
+height = float(input("Enter your height in meters: "))  # Converting input to float
+
+# Printing outputs
+print("\nUser Information:")
+print("Name:", name)
+print("Age:", age, "years")
+print("Height:", height, "meters")
+
