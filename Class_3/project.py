6dd7640c256211078849b0c9df0137f334fb4f14
Asus <mehhussumon47@gmail.com>
2025-06-06 03:07:39 +0600

    Last Folder Added

--- Class_3/project.py
+++ Class_3/project.py
@@ -1,9 +1,9 @@
 import random # python's Built-In library random
 
 # Generate a random number between 1 to 100
-number = random.randint(1, 100)
+secret_number = random.randint(1, 100)
 
-# print(number)
+# print(secret_number)
 print("Welcome to the Number Guessing Game!")
 print("I've picked a number between 1 to 100. Try to guess it!")
 
@@ -12,10 +12,10 @@ while (True):
     guess = int(input("Enter your guess: "))
 
     # Check the guess
-    if guess < number:
+    if guess < secret_number:
         print("Too low! Try again")
-    elif guess > number:
+    elif guess > secret_number:
         print("Too high! try again.")
     else:
-        print(f"Congratulations! You guessed the number {number} correctly!")
+        print(f"Congratulations! You guessed the number {secret_number} correctly!")
         break   # End the game
