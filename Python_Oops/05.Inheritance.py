3ca570aea63bc6fb51566cd9dfdc2c19461b31ae
MehrabHussain47 <mehhussumon47@gmail.com>
2025-06-09 19:39:45 +0600

    Another file added

---
+++ Python_Oops/05.Inheritance.py
@@ -0,0 +1,19 @@
+# In python support multiple inhertance
+class A:
+    def __init__(self):
+        print("Creating object for class A")
+    def jump(self):
+        print("I am jumping")
+
+class AA(A):    # A is parent class and AA is child class
+    def __init__(self): 
+        print("Creating object for class AA")
+    def jump(self): # Method overriding
+        print("Yay! I am jumping!!!") 
+
+a = A()
+# b = A()
+c = AA()
+
+a.jump()
+c.jump()
